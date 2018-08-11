1161ee143908031c5eb6935772311579850a8c32
Troy Olson <panreach@gmail.com>
2018-08-11 13:11:59 -0500

    release 3-7

--- upload/system/library/pdf_wizard/src/pdfs/OrderInfoPdf.php
+++ upload/system/library/pdf_wizard/src/pdfs/OrderInfoPdf.php
@@ -8,7 +8,7 @@ require_once("BasePdf.php");
  * @author     Original Author <support@monolithforge.com>
  * @copyright  2017-2018 Monolith Forge, LLC
  * @license    https://www.monolithforge.com/license/pdf-wizard-basic-license.txt
- * @version    3-6
+ * @version    3-7
  */
 class OrderInfoPdf extends BasePdf {
     
